3cf7cbedbaa208c9786807d8e3ef23d02c03eeee
adam krawiec <a.krawiec@exlabs.co.uk>
2024-02-10 23:29:07 +0100

    adds DTOs

--- Program.cs
+++ Program.cs
@@ -11,12 +11,25 @@ cities.Add(new City("Katowice"));
 cities.Add(new City("Tychy"));
 cities.Add(new City("Kraków"));
 
-List<RealEstate> realEstates = new List<RealEstate>();
-realEstates.Add(new RealEstate(new Address(streets[0], cities[0])));
-realEstates.Add(new RealEstate(new Address(streets[1], cities[0])));
-realEstates.Add(new RealEstate(new Address(streets[2], cities[0])));
-realEstates.Add(new RealEstate(new Address(streets[0], cities[1])));
 
+RealEstate re1 = new RealEstate(new Address(streets[0], cities[0]), 520);
+List<RealEstate> realEstates = [re1];
+realEstates.Add(new RealEstate(new Address(streets[1], cities[0]), 630));
+realEstates.Add(new RealEstate(new Address(streets[2], cities[0]), 820));
+realEstates.Add(new RealEstate(new Address(streets[0], cities[1]), 440));
+
+
+Staircase sc1 = new Staircase(1, re1);
+Staircase sc2 = new Staircase(2, re1);
+List<Staircase> staircases = [sc1, sc2];
+
+EstateUnit eu1 = new EstateUnit(1, sc1, 44.0f);
+EstateUnit eu2 = new EstateUnit(2, sc1, 56.0f);
+EstateUnit eu3 = new EstateUnit(1, sc2, 44.0f);
+EstateUnit eu4 = new EstateUnit(2, sc2, 56.0f);
+EstateUnit eu5 = new EstateUnit(2, sc2, 73.0f);
+
+List<EstateUnit> estateUnits = [eu1, eu2, eu3, eu4, eu5];
 
 var builder = WebApplication.CreateBuilder();
 builder.Services.AddAntiforgery();
@@ -36,20 +49,42 @@ app.MapGet("/real_estates", () => {
     return realEstates;
 });
 
-app.MapPost("/real_estates", ([FromForm] string streetName, [FromForm] string cityName) => {
+List<RealEstate> addRealEstate ([FromForm] string streetName, [FromForm] string cityName, [FromForm] float area) {
     Street? street = streets.Find(street => street.Name == streetName);
     City? city = cities.Find(city => city.Name == cityName);
 
     if (street != null && city != null) {
-        RealEstate realEstate = new RealEstate(new Address(street, city));
+        RealEstate realEstate = new RealEstate(new Address(street, city), area);
         realEstates.Add(realEstate);
     }
     
     return realEstates;
-}).DisableAntiforgery();
+}
 
+
+RealEstateDTO findRealEstate (int id) {
+    RealEstate? realEstate = realEstates.Find(realEstate => realEstate.Id == id);
+    if(realEstate is null) return new RealEstateDTO();
+
+    List<Staircase> realEstateStaircases = staircases.FindAll(staircase => staircase.RealEstate.Id == id);
+    
+    List<StaircaseDTO> staircaseDTOs = realEstateStaircases.Select((staircase) => {
+        IEnumerable<EstateUnitDTO> estateUnitDTOs = estateUnits.
+            FindAll(estateUnit => estateUnit.Staircase == staircase).
+            Select(estateUnit => new EstateUnitDTO(estateUnit));
+        return new StaircaseDTO(staircase, estateUnitDTOs.ToList());
+    }).ToList();
+
+    return new RealEstateDTO(realEstate, staircaseDTOs.ToList());
+}
+
+app.MapPost("/real_estates", addRealEstate).DisableAntiforgery();
 app.MapGet("/real_estates/{id}", (int id) => {
-    return realEstates.Find(realEstate => realEstate.Id == id);
+    RealEstateDTO realEstate = findRealEstate(id);
+
+    if (realEstate.Id is null) return Results.NotFound();
+
+    return Results.Ok(realEstate);
 });
 
 app.Run();
--- RealEstates.cs
+++ RealEstates.cs
@@ -29,11 +29,88 @@ namespace RealEstates {
         private static int _id = 0;
         public int Id { get; }
         public Address Address { get; set; }
+        public float Area { get; set; }
 
-        public RealEstate(Address address) {
+        public RealEstate(Address address, float area) {
             _id += 1;
             Id = _id;
             Address = address;
+            Area = area;
+        }
+    }
+
+    class Staircase {
+        private static int _id = 0;
+        public int Id { get; }
+        public int Number { get; set; }
+        public RealEstate RealEstate { get; set; }
+
+        public Staircase(int number, RealEstate realEstate) {
+            _id += 1;
+            Id = _id;
+            Number = number;
+            RealEstate = realEstate;
+        }
+    }
+
+    class EstateUnit {
+        private static int _id = 0;
+        public int Id { get; }
+
+        public int Number { get; set; }
+        public Staircase Staircase { get; set; }
+
+        public float Area { get; set; }
+
+        public EstateUnit(int number, Staircase staircase, float area) {
+            _id += 1;
+            Id = _id;
+            Number = number;
+            Staircase = staircase;
+            Area = area;
+        }
+    }
+
+    class RealEstateDTO {
+        public int? Id { get; }
+        public string? City { get; }
+        public string? Street { get; }
+        public List<StaircaseDTO>? Staircases { get; }
+
+        public RealEstateDTO() { 
+            Id = null;
+        }
+        public RealEstateDTO(RealEstate realEstate, List<StaircaseDTO>? staircases = null) {
+            Id = realEstate.Id;
+            City = realEstate.Address.City.Name;
+            Street = realEstate.Address.Street.Name;
+            Staircases = staircases is null ? [] : staircases;
+        }
+    };
+
+    class StaircaseDTO {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public List<EstateUnitDTO>? EstateUnits { get; set; }
+
+        public StaircaseDTO(Staircase staircase, List<EstateUnitDTO>? estateUnits = null) {
+            Id = staircase.Id;
+            Number = staircase.Number;
+            EstateUnits = estateUnits is null ?[] : estateUnits;
+        }
+    };
+
+    class EstateUnitDTO {
+        public int Id { get; set; }
+
+        public int Number { get; set; }
+
+        public float Area { get; set; }
+
+        public EstateUnitDTO(EstateUnit estateUnit) {
+            Id = estateUnit.Id;
+            Number = estateUnit.Number;
+            Area = estateUnit.Area;
         }
     }
 }
